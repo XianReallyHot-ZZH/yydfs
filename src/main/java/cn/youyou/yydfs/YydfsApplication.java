e55681c489054081c824174ba9ae3d79af361496
XianReallyHot-ZZH <741788769@qq.com>
2024-07-14 18:56:50 +0800

    feat:项目初始化

---
+++ src/main/java/cn/youyou/yydfs/YydfsApplication.java
@@ -0,0 +1,13 @@
+package cn.youyou.yydfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YydfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YydfsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/youyou/yydfs/YydfsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.youyou.yydfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YydfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
